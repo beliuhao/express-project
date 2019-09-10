60575211497603b121c48552c340269de6779bb9
Herman Liu <contactliuhao@gmail.com>
2019-09-10 11:14:30 +0800

    add data for testing routes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,47 @@ import cors from 'cors';
 
 const app = express();
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch'
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids'
+  }
+};
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1'
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2'
+  }
+};
+
 app.use(cors());
 
+// curl http://localhost:3000/users
 app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
 });
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+// curl - X POST http://localhost:3000
 app.post('/users', (req, res) => {
   return res.send('POST HTTP method on user resource');
 });
+// curl - X PUT http://localhost:3000
 app.put('/users/:userId', (req, res) => {
   return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
 });
+// curl - X DELETE http://localhost:3000
 app.delete('/users/:userId', (req, res) => {
   return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
